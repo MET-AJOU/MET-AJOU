de8601f5188da2c3caad31bd39182820e0f243a6
jin-Pro <dnjun2@ajou.ac.kr>
2021-12-24 01:14:43 +0900

    Feat : First Test Work

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,87 @@
-import React from 'react';
-import * as THREE from 'three';
+import React,{useState, useEffect,useLayoutEffect} from 'react';
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import Stats from 'three/examples/jsm/libs/stats.module'
+
+let count = 0
 
 function App() {
-  const canvas = document.querySelector("canvas.webgl");
-const scene = new THREE.Scene();
+  const [dis,setDis] = useState(2);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+  const scene = new THREE.Scene()
 
-const sizes = {
-  width: 800,
-  height: 600,
-};
+// 모델 불러오기
+  const loader = new GLTFLoader()
+// 모델 함수
+  loader3D(loader,scene,'./models/chair.glb')
+  loader3D(loader,scene,'./models/counter.glb')
+  loader3D(loader,scene,'./models/sofa.glb')
+  loader3D(loader,scene,'./models/table.glb')
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 50;
-scene.add(camera);
 
-const renderer = new THREE.WebGLRenderer({
-  canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
+// 조명
+  const light = new THREE.DirectionalLight(0xffffff,1)
+// 조명 위치
+  light.position.set(2,2,5)
+// 조명 추가
+  scene.add(light)
 
-renderer.render(scene, camera);
+  const size = {
+    width : window.innerWidth,
+    height : window.innerHeight,
+  }
+
+  const camera = new THREE.PerspectiveCamera(
+      80,
+      size.width / size.height,
+      0.1,
+      100
+  )
+  // 카메라 바라보는 시선 위치? (좌,우 : 커질수록 오른쪽으로) , (위,아래 : 커질수록 올라감) , (거리 : 커질수록 멀어짐)
+  camera.position.set(0,0.5,dis)
+  scene.add(camera)
+
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize(size.width,size.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio,2));
+  renderer.shadowMap.enabled = true
+  renderer.gammmaOutput = true
+
+  document.body.innerHTML = '';
+  document.body.appendChild(renderer.domElement)
+
+  function animate(){
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+  }
+
+  animate()
+
+  // 내 의도는 키 이벤트 발생 => dis 수정 => camera위치 이동
+  // useLayoutEffect(() => {
+  //   camera.position.set(0,0.5,dis)
+  //   scene.add(camera)
+  //   renderer.render(scene, camera)
+  // },[dis])
+
+  useEffect(() => {
+    window.addEventListener('keydown',move)
+  },[dis])
+
+  useEffect(() => {
+    document.body.innerHTML = '';
+    document.body.appendChild(renderer.domElement)
+  },[])
+
+  function move(e) {
+    if(e.key === 'ArrowUp'){
+      setDis(prev => prev + 0.1)
+    }
+    if(e.key === 'ArrowDown'){
+      setDis(prev => prev - 0.1)
+    }
+  }
 
   return(
     null
@@ -32,3 +89,22 @@ renderer.render(scene, camera);
 }
 
 export default App;
+
+function loader3D(loader,scene,url){
+  // load가 비동기라서 requestAnimationFrame이 들어감
+  // 동기적으로 할 수 있으면 requestAnimationFrame안해도 될거같은데
+  loader.load(
+      url,
+      function (glb) {
+        const root = glb.scene;
+        root.scale.set(0.12,0.12,0.12)
+        scene.add(root)
+      },
+      (xhr) => {
+          console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+      },
+      (error) => {
+          console.log(error)
+      }
+  )
+}
