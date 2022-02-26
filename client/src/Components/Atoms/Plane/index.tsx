1dc2a7509fde0bccff821ad08f43deecbccbcad5
Noelsky <hansorly@ajou.ac.kr>
2022-02-27 01:14:41 +0900

    Fix : 버그 픽스용 Plane  생성

--- client/src/Components/Atoms/Plane/index.tsx
+++ client/src/Components/Atoms/Plane/index.tsx
@@ -1,19 +1,16 @@
-import { usePlane, BoxProps } from "@react-three/cannon";
-import { useGLTF } from "@react-three/drei";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { PlaneProps, usePlane } from "@react-three/cannon";
 // import * as THREE from "three";
 
-interface Props {
-  src: string;
-  // position: number[];
-}
-const Plane = ({ src, position = [0, 0, 0], rotation = [0, -0.09, 0] }: BoxProps & Props) => {
-  // const Building = ({ src, position }: Props) => {
-  const { nodes } = useGLTF(src);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [ref] = usePlane(() => ({ position, rotation }), undefined, [position, rotation]);
+const Plane = ({ args, position = [0, 0, 0], rotation = [-Math.PI / 2, 0, -0.1] }: PlaneProps) => {
+  const [ref] = usePlane(() => ({ mass: 10, position, rotation, type: "Static" }), undefined, [position, rotation]);
   // const { nodes, materials } = useGLTF(src);
-  const nodeKey = Object.keys(nodes)[0];
-  return <mesh ref={ref} castShadow receiveShadow geometry={(nodes[nodeKey] as any).geometry} material={(nodes[nodeKey] as any).material} />;
+  return (
+    <mesh ref={ref} castShadow visible>
+      <planeGeometry args={args as any} />
+      <meshStandardMaterial color="blue" />
+    </mesh>
+  );
   // return <mesh position={new THREE.Vector3(...position)} castShadow receiveShadow geometry={(nodes[nodeKey] as any).geometry} material={(nodes[nodeKey] as any).material} />;
 };
 
