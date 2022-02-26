152c74902b846f24931cadf7fee3e0715e579e92
Noelsky <hansorly@ajou.ac.kr>
2022-02-27 01:14:54 +0900

    Fix : 버그 픽스용 box 생성

--- client/src/Components/Atoms/Building/test.tsx
+++ client/src/Components/Atoms/Building/test.tsx
@@ -1,12 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useBox } from "@react-three/cannon";
+import { useBox, BoxProps } from "@react-three/cannon";
+import { useGLTF } from "@react-three/drei";
+import { useEffect } from "react";
+// import * as THREE from "three";
 
-const Cube = (props: any) => {
-  const [ref] = useBox(() => ({ mass: 1, position: [-5, 2, 0], args: [2, 1, 2] }));
+const Box = ({ args = [5, 2.3, 2], position = [0, 0, 0], rotation = [0, -0.09, 0] }: BoxProps) => {
+  const [ref, api] = useBox(() => ({ mass: 100, type: "Static", args, position, rotation }), undefined, [args, position, rotation]);
+
+  // 아래는 테스트용 절대 지우지 마셈
   return (
-    <mesh ref={ref}>
-      <boxGeometry />
+    <mesh castShadow position={position} ref={ref}>
+      <boxBufferGeometry attach="geometry" args={args} />
+      <meshStandardMaterial color="black" />
     </mesh>
   );
+  // return <mesh position={new THREE.Vector3(...position)} castShadow receiveShadow geometry={(nodes[nodeKey] as any).geometry} material={(nodes[nodeKey] as any).material} />;
 };
-export default Cube;
+
+export default Box;
