cc307198d3b7a38668f5eb7a41a08af3be01b68f
Noelsky <hansorly@ajou.ac.kr>
2022-03-04 22:31:36 +0900

    Feat : 키보드 상태  타입, atom 추가

---
+++ client/src/Common/Constant/Three/index.ts
@@ -0,0 +1,9 @@
+import { keyBoardStateType } from "@Type/Three";
+
+export const DefaultKeyboardState: keyBoardStateType = {
+  backward: false,
+  forward: false,
+  left: false,
+  right: false,
+  boost: false,
+};
