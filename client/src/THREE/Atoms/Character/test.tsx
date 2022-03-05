804ef31083761f09272f43a98867d9ecf2e45716
Noelsky <hansorly@ajou.ac.kr>
2022-03-05 14:52:43 +0900

    Feat : 캐릭터 이동 구현 v1

--- client/src/THREE/Atoms/Character/test.tsx
+++ client/src/THREE/Atoms/Character/test.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-nested-ternary */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useBox, useRaycastAll, useRaycastVehicle } from "@react-three/cannon";
+import { useBox, useRaycastAll, useRaycastVehicle, useSphere } from "@react-three/cannon";
 import { useFrame, useThree } from "@react-three/fiber";
 import { keyBoardStateAtom, positionAtom } from "@Recoils/.";
 import { PositionType } from "@Type/.";
@@ -11,29 +11,41 @@ import { Vector3 } from "three";
 
 const TestCharacter = (props: any) => {
   // const [ref] = useRaycastVehicle();
+
+  const [ref, api] = useSphere(() => ({ mass: 10, position: [0, 1, 0], type: "Dynamic" }));
+
   const { forward, backward, left, right, boost } = useRecoilValue<keyBoardStateType>(keyBoardStateAtom);
 
   const { camera } = useThree();
 
-  let speed = 0;
-  const vector = new Vector3();
+  const fowardVector = new Vector3();
+  const sideVector = new Vector3();
+  const direction = new Vector3();
+  const characterPosition = new Vector3();
+  let fowardSpeed = 0;
+  let sideSpeed = 0;
 
   useEffect(() => {
-    camera.position.set(0, 10, 10);
+    camera.position.set(0, 5, 5);
     camera.lookAt(ref.current!.position);
   }, [camera]);
 
   useFrame((state, delta) => {
-    speed = forward || backward ? 0.1 * (forward && !backward ? (boost ? 1.5 : 1) : boost ? -1.5 : -1) : 0;
-    vector.set(0, speed, 0);
-    camera.posi;
+    fowardSpeed = forward || backward ? -0.1 * (forward && !backward ? (boost ? 1.5 : 1) : boost ? -1.5 : -1) : 0;
+    fowardVector.set(0, 0, fowardSpeed);
+    sideSpeed = left || right ? -0.1 * (right ? 1 : -1) : 0;
+    sideVector.set(sideSpeed, 0, 0);
+    direction.subVectors(fowardVector, sideVector).normalize().multiplyScalar(1);
+    api.velocity.set(direction.x, 0, direction.z);
+    ref.current!.getWorldPosition(characterPosition);
+    camera.lookAt(characterPosition);
   });
 
   // eslint-disable-next-line react/destructuring-assignment
   return (
     <mesh castShadow ref={ref}>
       <boxBufferGeometry attach="geometry" args={[0.1, 0.1, 0.1]} />
-      <meshStandardMaterial color="ref" />
+      <meshStandardMaterial color="green" />
     </mesh>
   );
 };
