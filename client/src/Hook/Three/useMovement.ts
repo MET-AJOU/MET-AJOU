bd8b84454e69194198a930d849202c1c97b479f6
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-03-13 17:38:18 +0900

    Feat : 점프 구현중

--- client/src/Hook/Three/useMovement.ts
+++ client/src/Hook/Three/useMovement.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-nested-ternary */
 import { useThree, useFrame } from "@react-three/fiber";
 import { keyBoardStateAtom } from "@Recoils/.";
@@ -8,7 +10,7 @@ import { Vector3 } from "three";
 
 const SPEED = -1;
 
-const useCharacterMovement = ({ api, ref, actions }: { api: any; ref: any; actions: any }) => {
+const useCharacterMovement = ({ api, ref, actions, canJump }: { api: any; ref: any; actions: any; canJump: any }) => {
   const { forward, backward, left, right, boost, space } = useRecoilValue<keyBoardStateType>(keyBoardStateAtom);
 
   const { camera } = useThree();
@@ -18,11 +20,14 @@ const useCharacterMovement = ({ api, ref, actions }: { api: any; ref: any; actio
   const direction = new Vector3();
   const characterPosition = new Vector3();
   const cameraPosition = new Vector3();
+  const temp = new Vector3();
   let fowardSpeed = 0;
   let sideSpeed = 0;
   let upwardSpeed = 0;
   let boostSpeed = 1;
   let characterDir = 0;
+  //   let upwardTime = 0;
+  console.log("re");
 
   useFrame((state, delta) => {
     if (!forward && !backward && !left && !right) {
@@ -31,19 +36,34 @@ const useCharacterMovement = ({ api, ref, actions }: { api: any; ref: any; actio
       actions["mixamo.com"].play();
     }
 
+    // console.log(delta);
+    // console.log(upwardTime);
+    // console.log(api.collisionResponse.set());
+    // api.collisionResponse(() => {
+    //   console.log(123);
+    // });
     characterDir = forward || left || right ? (forward ? Math.PI : backward ? Math.PI : left ? (3 * Math.PI) / 2 : Math.PI / 2) : 0;
     fowardSpeed = forward || backward ? (forward && !backward ? 1 : -1) : 0;
     fowardVector.set(0, 0, fowardSpeed);
     sideSpeed = left || right ? (right ? 1 : -1) : 0;
     sideVector.set(sideSpeed, 0, 0);
     // upwardSpeed = upwardSpeed === 3 ? (space ? 3 : -1) : -1;
-    upwardSpeed = space ? 3 : -1;
+    // console.log(canJump);
+    console.log(api);
+    if (canJump && space) {
+      upwardSpeed = 10;
+      canJump = false;
+    }
     boostSpeed = boost ? 2 : 1;
     direction
       .subVectors(fowardVector, sideVector)
       .normalize()
       .multiplyScalar(SPEED * boostSpeed);
     api.velocity.set(direction.x, upwardSpeed, direction.z);
+    // ref.current!.getWorldDirection(temp);
+    // console.log(temp.y);
+
+    // console.log(ref.current.velocity);
     api.rotation.set(0, characterDir, 0);
     ref.current!.getWorldPosition(characterPosition);
     camera.lookAt(characterPosition);
