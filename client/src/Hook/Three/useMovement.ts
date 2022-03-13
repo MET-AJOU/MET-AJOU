91f61e8521e0bdf87c8698d5e79fdf79a985c942
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-03-13 15:32:22 +0900

    Fix : 구 args 수정

--- client/src/Hook/Three/useMovement.ts
+++ client/src/Hook/Three/useMovement.ts
@@ -6,10 +6,9 @@ import { keyBoardStateType } from "@Type/Three";
 import { useRecoilValue } from "recoil";
 import { Vector3 } from "three";
 
-const SPEED = -2;
+const SPEED = -1;
 
 const useCharacterMovement = ({ api, ref, actions }: { api: any; ref: any; actions: any }) => {
-  // const { forward, backward, left, right, boost } = useRecoilValue<keyBoardStateType>(keyBoardStateAtom);
   const { forward, backward, left, right, boost, space } = useRecoilValue<keyBoardStateType>(keyBoardStateAtom);
 
   const { camera } = useThree();
@@ -19,37 +18,13 @@ const useCharacterMovement = ({ api, ref, actions }: { api: any; ref: any; actio
   const direction = new Vector3();
   const characterPosition = new Vector3();
   const cameraPosition = new Vector3();
-  // const temp = new Vector3();
   let fowardSpeed = 0;
   let sideSpeed = 0;
   let upwardSpeed = 0;
   let boostSpeed = 1;
   let characterDir = 0;
 
-  // useEffect(() => {
-  //   camera.position.set(0, 5, 5);
-  //   camera.lookAt(ref.current!.position);
-  // }, [camera]);
-
-  // useEffect(() => {
-  //   if (!forward && !backward && !left && !right) {
-  //     actions["mixamo.com"].stop();
-  //     return;
-  //   }
-  //   actions["mixamo.com"].play();
-  // }, [forward, backward, left, right]);
-  // useEffect(() => {
-  //   actions["mixamo.com"].play();
-  //   // actions["Take 001"].play();
-  //   // actions.name["mixamo.com"].play();
-  // }, [actions]);
-  console.log(direction);
-  console.log(upwardSpeed);
-  console.log(api);
-
   useFrame((state, delta) => {
-    // console.log(api.rotation.set(0, 0, 0));
-    // console.log(temp);
     if (!forward && !backward && !left && !right) {
       actions["mixamo.com"].stop();
     } else {
@@ -61,6 +36,7 @@ const useCharacterMovement = ({ api, ref, actions }: { api: any; ref: any; actio
     fowardVector.set(0, 0, fowardSpeed);
     sideSpeed = left || right ? (right ? 1 : -1) : 0;
     sideVector.set(sideSpeed, 0, 0);
+    // upwardSpeed = upwardSpeed === 3 ? (space ? 3 : -1) : -1;
     upwardSpeed = space ? 3 : -1;
     boostSpeed = boost ? 2 : 1;
     direction
