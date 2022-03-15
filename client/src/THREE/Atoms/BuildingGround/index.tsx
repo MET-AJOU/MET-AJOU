ea1b46617f911ee77fd7e097fd7f846fb20c99b0
Noelsky <hansorly@ajou.ac.kr>
2022-03-15 17:11:35 +0900

    Feat : Building Grounds asset 렌더링

---
+++ client/src/THREE/Atoms/BuildingGround/index.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useBox, BoxProps } from "@react-three/cannon";
+import { useGLTF } from "@react-three/drei";
+
+interface Props {
+  src: string;
+}
+const BuildingGround = ({ src, args, position = [0, 0, 0], rotation = [0, -0.09, 0] }: BoxProps & Props) => {
+  const { nodes } = useGLTF(src);
+
+  const [ref] = useBox(() => ({ mass: 100, type: "Static", args, position, rotation }), undefined, [args, position, rotation]);
+  console.log(nodes);
+  const { geometry, material } = nodes.building_ground.children[0] as any;
+
+  return <mesh ref={ref} castShadow receiveShadow geometry={geometry} material={material} />;
+};
+
+export default BuildingGround;
