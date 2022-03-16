4f0fbcf1c1c8d5b757fbcec85c09b9402ccafd96
Noelsky <hansorly@ajou.ac.kr>
2022-03-16 15:55:51 +0900

    Feat : 3번 블록까지 fake plane

--- client/src/THREE/Atoms/Character/test.tsx
+++ client/src/THREE/Atoms/Character/test.tsx
@@ -15,7 +15,7 @@ const TestCharacter = ({ src }: { src: string }) => {
   const [ref, api] = useSphere(() => ({
     mass: 100,
     args: [0.1],
-    position: [-3, 5, 25],
+    position: [13, 5, 21],
     type: "Dynamic",
   }));
 
