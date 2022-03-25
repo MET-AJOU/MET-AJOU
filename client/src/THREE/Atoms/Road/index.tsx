78d86b8505a55090f569bda7116f9b1a5ab8c55e
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-03-25 19:22:54 +0900

    Feat : fake plane ver 2

--- client/src/THREE/Atoms/Road/index.tsx
+++ client/src/THREE/Atoms/Road/index.tsx
@@ -1,18 +1,34 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useBox, BoxProps } from "@react-three/cannon";
+import { useBox, BoxProps, useConvexPolyhedron } from "@react-three/cannon";
 import { useGLTF } from "@react-three/drei";
+import { threeToCannon } from "three-to-cannon";
 
 interface Props {
   src: string;
 }
+
+const makeVertices = (vertices: any[]): any => vertices.map((vertice: any) => [vertice.x, vertice.y, vertice.z]);
+
 const Road = ({ src, args, position = [0, 0, 0], rotation = [0, -0.09, 0] }: BoxProps & Props) => {
   const { nodes } = useGLTF(src);
 
-  const [ref] = useBox(() => ({ mass: 100, type: "Static", args, position, rotation }), undefined, [args, position, rotation]);
+  // const [ref] = useBox(() => ({ mass: 100, type: "Static", args, position, rotation }), undefined, [args, position, rotation]);
+  const { shape, offset } = threeToCannon(nodes.road as any) as any;
+  const {
+    convexPolyhedronRepresentation: { vertices, faces, faceNormals: normals, uniqueAxes: axes },
+    boundingSphereRadius,
+  } = shape;
+
+  const [a] = useConvexPolyhedron(() => ({ type: "Static", position: [offset.x, offset.y + 1.2, offset.z], args: [makeVertices(vertices), faces, makeVertices(normals), makeVertices(axes), boundingSphereRadius], mass: 100 }), undefined, [nodes]);
 
   const { geometry, material } = nodes.road.children[0] as any;
 
-  return <mesh ref={ref} castShadow receiveShadow geometry={geometry} material={material} />;
+  return (
+    <>
+      <mesh castShadow position={position} receiveShadow geometry={geometry} material={material} />
+      <mesh ref={a} />
+    </>
+  );
 };
 
 export default Road;
