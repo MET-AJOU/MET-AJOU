4a5ffc8e8555992ecd500ec81f2297b7eb0fa33f
Noelsky <hansorly@ajou.ac.kr>
2022-03-29 01:05:16 +0900

    Feat : 소켓 컴포넌트 추가

--- client/src/Components/Pages/Map.tsx
+++ client/src/Components/Pages/Map.tsx
@@ -1,10 +1,7 @@
 import { Suspense } from "react";
 import MapContainer from "@THREE/Templates/Map";
-import useInitSocket from "@Socket/Hook/useInitSocket";
 
 const MapPage = () => {
-  useInitSocket();
-
   return (
     <Suspense fallback={null}>
       <MapContainer />
