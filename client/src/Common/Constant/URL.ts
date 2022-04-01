8b8fccc88e030b6d8ed34ae334e1d0d0fb7db7c5
jin-Pro <dnjun2@ajou.ac.kr>
2022-04-02 02:23:11 +0900

    Fix

--- client/src/Common/Constant/URL.ts
+++ client/src/Common/Constant/URL.ts
@@ -10,9 +10,9 @@ export const AUTH_SERVER = `${REACT_APP_GET_SERVER_DOMAIN}${REACT_APP_GET_SERVER
 export const SOCKET_SERVER = `${REACT_APP_SOCKET_SERVER_URL}:${REACT_APP_SOCKET_SERVER_PORT}`;
 // export const AUTH_SERVER = `${process.env.REACT_APP_GET_SERVER_DOMAIN}${process.env.REACT_APP_GET_SERVER_PORT}`;
 // export const SOCKET_SERVER = `${process.env.REACT_APP_SOCKET_SERVER_URL}:${process.env.REACT_APP_SOCKET_SERVER_PORT}`;
+// export const GOOGLE_URL = `${AUTH_SERVER}${process.env.REACT_APP_GET_GOOGLE_URL}`;
 
 export const GOOGLE_URL = `${AUTH_SERVER}${REACT_APP_GET_GOOGLE_URL}`;
-// export const GOOGLE_URL = `${AUTH_SERVER}${process.env.REACT_APP_GET_GOOGLE_URL}`;
 export const CHECK_TOKEN = `${AUTH_SERVER}/api/token/mine`;
 export const GET_VERIFY_EMAIL = `${AUTH_SERVER}/api/verify/send/ajouemail`;
 export const CHECK_EXPIRED = `${AUTH_SERVER}/api/token/expiredtime`;
