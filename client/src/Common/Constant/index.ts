e2e873f8d2bc2c95b49d5d37b3b1f5e651c4b643
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-04-27 19:33:11 +0900

    Feat : svg 추가 및 데 등록

--- client/src/Common/Constant/index.ts
+++ client/src/Common/Constant/index.ts
@@ -1,5 +1,32 @@
 /* eslint-disable no-loss-of-precision */
 
+import { Info1, Info2, Info3, Info4, Info5, Info6 } from "@Atoms/InfoSvgs";
+
 export const hello = () => "hello";
 
-export const AJOU_LOGO_SRC = "/asset/Main/Top/ajou_logo.svg";
+export const MAIN_INFO_DATA = [
+  {
+    Element: Info1,
+    text: ["미니맵과 영상을 통해", "쉽고 재밌는 학교 정보 열람하기"],
+  },
+  {
+    Element: Info2,
+    text: ["재학생들만 접속할 수 있는", "시스템 구축"],
+  },
+  {
+    Element: Info3,
+    text: ["3d 가상세계 속", "아주대학교 캠퍼스 재현"],
+  },
+  {
+    Element: Info4,
+    text: ["문자, 음성채팅, 감정표현 등", "다양한 실시간 의사소통 방식 제공"],
+  },
+  {
+    Element: Info5,
+    text: ["캐릭터 커스터마이징을 이용해", "나만의 캐릭터 만들기"],
+  },
+  {
+    Element: Info6,
+    text: ["목적에 맞는", "룸 채널 선택 & 접속"],
+  },
+];
---
+++ client/src/Components/Atoms/InfoSvgs/index.tsx
@@ -0,0 +1,8 @@
+import { ReactComponent as Info1 } from "@Asset/info1.svg";
+import { ReactComponent as Info2 } from "@Asset/info2.svg";
+import { ReactComponent as Info3 } from "@Asset/info3.svg";
+import { ReactComponent as Info4 } from "@Asset/info4.svg";
+import { ReactComponent as Info5 } from "@Asset/info5.svg";
+import { ReactComponent as Info6 } from "@Asset/info6.svg";
+
+export { Info1, Info2, Info3, Info4, Info5, Info6 };
