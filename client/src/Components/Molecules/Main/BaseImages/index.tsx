45ac9ca6c7ef2d3e5ccf4c2b75f16c57c07c8c24
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-04-28 16:40:08 +0900

    Fix : 11

--- client/src/Components/Molecules/Main/BaseImages/index.tsx
+++ client/src/Components/Molecules/Main/BaseImages/index.tsx
@@ -2,16 +2,16 @@
 import { useInView } from "react-hook-inview";
 
 import { BaseImageProps } from "@Type/.";
-import { BaseImage } from "./styles";
+import { Container, BaseImage } from "./styles";
 
 const BaseImages = ({ data }: { data: BaseImageProps[] }) => {
   const [ref, isVisible] = useInView();
   return (
-    <div ref={ref}>
+    <Container ref={ref}>
       {data.map((ImageStyle: BaseImageProps, idx) => (
         <BaseImage className={isVisible ? "down" : "up"} key={`baseimage +${ImageStyle.url}`} alt={`baseimage${idx}`} src={ImageStyle.url} {...{ ...ImageStyle, idx }} />
       ))}
-    </div>
+    </Container>
   );
 };
 
