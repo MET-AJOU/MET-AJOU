4106265b0d7bbe06763b39e08cc754c525ba2799
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-04-30 00:24:33 +0900

    Fix : 랜딩페이지 물결 애니메이션 수정

--- client/src/Components/Molecules/Main/BaseImages/index.tsx
+++ client/src/Components/Molecules/Main/BaseImages/index.tsx
@@ -6,10 +6,11 @@ import { Container, BaseImage } from "./styles";
 
 const BaseImages = ({ data, className }: { data: BaseImageProps[]; className: string }) => {
   const [ref, isVisible] = useInView();
+  const defaultIndex = className === "top" ? 5 : 0;
   return (
     <Container ref={ref}>
       {data.map((ImageStyle: BaseImageProps, idx) => (
-        <BaseImage className={className} key={`baseimage +${ImageStyle.url}`} alt={`baseimage${idx}`} src={ImageStyle.url} {...{ ...ImageStyle, idx }} />
+        <BaseImage className={idx === 0 ? "none" : className} key={`baseimage +${ImageStyle.url}`} alt={`baseimage${idx}`} src={ImageStyle.url} {...{ ...ImageStyle, idx: defaultIndex + idx }} />
       ))}
     </Container>
   );
--- client/src/Components/Molecules/Main/BaseImages/styles.tsx
+++ client/src/Components/Molecules/Main/BaseImages/styles.tsx
@@ -1,19 +1,18 @@
 import styled from "styled-components";
 import { BaseImageProps } from "@Type/.";
 
-export const Container = styled.div``;
-export const BaseImage = styled.img<BaseImageProps & { idx: number }>`
+export const Container = styled.div`
   @keyframes waves {
     from {
       height: 900px;
     }
     50% {
-      width: 130vw;
       height: 800px;
-      top: -20px;
+      margin-top: -100px;
     }
     to {
       height: 900px;
+      margin-top: 0px;
     }
   }
 
@@ -25,16 +24,18 @@ export const BaseImage = styled.img<BaseImageProps & { idx: number }>`
       height: 800px;
     }
   }
+  & .top {
+    animation: waves 7s infinite;
+    -webkit-animation: waves 7s infinite;
+  }
+`;
 
+export const BaseImage = styled.img<BaseImageProps & { idx: number }>`
   position: absolute;
   width: ${({ width }) => width};
   top: ${({ top }) => top};
-  z-index: ${({ idx }) => -idx - 4};
+  z-index: ${({ idx }) => -idx - 8};
   ${({ transform }) => transform && `transform:${transform}`}
   ${({ boxShadow }) => boxShadow && `box-shadow:${boxShadow}`}
   left: ${({ left }) => left};
-  & .top {
-    animation: waves 10s infinite;
-    -webkit-animation: waves 10s infinite;
-  }
 `;
