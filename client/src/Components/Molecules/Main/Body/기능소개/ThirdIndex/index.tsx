319055e47a2cd7d311bc54e1699cbeeaa052945e
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-04-30 03:04:33 +0900

    Feat : 랜딩 - 하트,파도,선구  애니메이션

--- client/src/Components/Molecules/Main/Body/기능소개/ThirdIndex/index.tsx
+++ client/src/Components/Molecules/Main/Body/기능소개/ThirdIndex/index.tsx
@@ -1,11 +1,32 @@
+import { useInView } from "react-hook-inview";
+import { useEffect } from "react";
+
 import BlueText from "@Atoms/BlueText";
 import StartButton from "@Atoms/StartButton";
 import { LandingBottomWave2, LandingBottomWave3, LandingBottomWave4, Heart1, Heart2, SeonGu, Moon, LandingBottomWave1 } from "@Atoms/Svgs";
 // import BaseImages from "@Molecules/Main/BaseImages";
 // import { MAIN_BOTTOM_WAVE_IMAGES } from "@Style/.";
-import { Wave1Style, Wave2Style, Wave3Style, Wave4Style, Container, InfoContainer, Title, Text, Heart1Style, Heart2Style, SeonGuStyle, MoonStyle } from "./styles";
+import { Wave1Style, Wave2Style, Wave3Style, Wave4Style, Container, InfoContainer, Title, Text, SeonGuStyle, MoonStyle } from "./styles";
 
 const ThirdIndex = () => {
+  const [ref, isVisible] = useInView();
+
+  useEffect(() => {
+    if (!isVisible) return;
+    const childrens = document.querySelectorAll(".hearts");
+    if (!childrens) return;
+
+    setTimeout(() => {
+      childrens[0].classList.add("hearts1");
+      childrens[0].classList.remove("hide_up");
+    }, 0);
+
+    setTimeout(() => {
+      childrens[1].classList.add("hearts2");
+      childrens[1].classList.remove("hide_up");
+    }, 1000);
+  }, [isVisible]);
+
   return (
     <Container>
       <InfoContainer>
@@ -17,15 +38,17 @@ const ThirdIndex = () => {
         <Text>지금 바로 접속해보세요!</Text>
         <StartButton color="blue" />
       </InfoContainer>
-      <Heart1 style={Heart1Style} />
-      <Heart2 style={Heart2Style} />
+      <div ref={ref}>
+        <Heart1 className="hearts hide_up" />
+        <Heart2 className="hearts hide_up" />
+      </div>
       <SeonGu style={SeonGuStyle} />
       <Moon style={MoonStyle} />
       {/* <BaseImages data={MAIN_BOTTOM_WAVE_IMAGES} /> */}
-      <LandingBottomWave1 style={Wave1Style} />
-      <LandingBottomWave2 style={Wave2Style} />
-      <LandingBottomWave3 style={Wave3Style} />
-      <LandingBottomWave4 style={Wave4Style} />
+      <LandingBottomWave1 className="waves" style={Wave1Style} />
+      <LandingBottomWave2 className="waves" style={Wave2Style} />
+      <LandingBottomWave3 className="waves" style={Wave3Style} />
+      <LandingBottomWave4 className="waves" style={Wave4Style} />
     </Container>
   );
 };
