991c5b684a99edc1a434ce838feb5857f38379d7
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-04-30 00:49:33 +0900

    Feat : First Info 애니메이션 추가

--- client/src/Components/Atoms/InfoCard/styles.tsx
+++ client/src/Components/Atoms/InfoCard/styles.tsx
@@ -3,11 +3,13 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100%;
+  min-height: 100%;
+  min-width: 100%;
   display: flex;
   flex-direction: column;
   padding: auto;
   box-shadow: inset 20px 20px 10px rgba(0, 0, 0, 0.1);
-  font-weight: 400;
+  font-weight: 600;
   font-style: normal;
   font-size: 12px;
   line-height: 24px;
@@ -15,4 +17,16 @@ export const Container = styled.div`
   align-items: center;
   justify-content: center;
   border-radius: 16px;
+
+  .up {
+    position: relative;
+    top: 0px;
+    opacity: 1;
+    transition: 1s;
+  }
+  .down {
+    position: absolute;
+    top: 200px;
+    opacity: 0;
+  }
 `;
--- client/src/Components/Molecules/Main/BaseImages/index.tsx
+++ client/src/Components/Molecules/Main/BaseImages/index.tsx
@@ -1,14 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { useInView } from "react-hook-inview";
-
 import { BaseImageProps } from "@Type/.";
 import { Container, BaseImage } from "./styles";
 
 const BaseImages = ({ data, className }: { data: BaseImageProps[]; className: string }) => {
-  const [ref, isVisible] = useInView();
   const defaultIndex = className === "top" ? 5 : 0;
   return (
-    <Container ref={ref}>
+    <Container>
       {data.map((ImageStyle: BaseImageProps, idx) => (
         <BaseImage className={idx === 0 ? "none" : className} key={`baseimage +${ImageStyle.url}`} alt={`baseimage${idx}`} src={ImageStyle.url} {...{ ...ImageStyle, idx: defaultIndex + idx }} />
       ))}
