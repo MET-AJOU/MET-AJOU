b3e3a36c37b1bb2e7b0b3e100714bead7266700c
Noelsky <hansorly@ajou.ac.kr>
2022-05-03 21:26:04 +0900

    Refactor : 애니메이션 스타일 분리

---
+++ client/src/Common/Style/animations/slide.tsx
@@ -0,0 +1,63 @@
+const SlideAnimations = `
+.slide_up {
+  position: relative;
+  top: 0px;
+  opacity: 1;
+  transition: 1s;
+}
+
+.slide_down_relative {
+  position:relative;
+  top:200px;
+  opacity:0;
+  transition:1s;
+}
+.hide {
+  opacity: 0;
+  transition: 1s;
+}
+.slide_up_no_pos {
+  opacity: 1;
+  transition: 1s;
+}
+.slide_up_opacity {
+  position: relative;
+  top: 0px;
+  opacity: 0.3;
+  transition: 1s;
+}
+
+.slide_down {
+  position: absolute;
+  top: 200px;
+  opacity: 0;
+}
+.slide_down_middle{
+  position:absolute !important;
+  top: 380vh !important; 
+  opacity:0;
+}
+.slide_down_bottom {
+  position:absolute !important;
+  top: 450vh !important; 
+  opacity:0;
+}
+
+.hide_up {
+  position:relative;
+  top:-10vh;
+  opacity:0;
+}
+
+.scale_down{
+  width: 0%!important;
+}
+
+.stand_left{
+  position:relative;
+  left:-45vw;
+  top: -14vh;
+  opacity:0;
+}`;
+
+export default SlideAnimations;
---
+++ client/src/Common/Style/animations/waves.tsx
@@ -0,0 +1,83 @@
+const WaveAnimations = `
+
+@keyframes about_waves {
+  from {
+    
+  }
+  50% {
+    width:101vw;
+    margin-top : -4vh;
+  }
+  to {
+
+  }
+}
+
+@-webkit-keyframes about_waves {
+  from {
+    
+  }
+  50% {
+    margin-top : -20px;
+  }
+  to {
+
+  }
+}
+
+@keyframes waves {
+  from {
+    height: 900px;
+  }
+  50% {
+    height: 800px;
+    margin-top: -100px;
+  }
+  to {
+    height: 900px;
+    margin-top: 0px;
+  }
+}
+
+@-webkit-keyframes waves {
+  from {
+    height: 900px;
+  }
+  50% {
+    height: 800px;
+    margin-top: -100px;
+  }
+  to {
+    height: 900px;
+    margin-top: 0px;
+  }
+}
+
+@keyframes bottom_waves {
+  from{
+    
+  }
+  50%{
+    width:150vw;
+    margin-top: -20px;
+  }
+  to{
+    
+  }
+}
+
+@-webkit-keyframes bottom_waves {
+  from{
+
+  }
+  50%{
+    width:150vw;
+    margin-top: -20px;
+  }
+  to{
+    
+  }
+}
+`;
+
+export default WaveAnimations;
