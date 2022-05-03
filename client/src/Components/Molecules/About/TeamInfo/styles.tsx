7cbcee79906113e7883c3dde8e7812f3e23bc63c
Noelsky <hansorly@ajou.ac.kr>
2022-05-03 21:26:50 +0900

    Feat : 팀 사진 컴포넌트 구현

---
+++ client/src/Components/Molecules/About/TeamInfo/styles.tsx
@@ -0,0 +1,14 @@
+import { CenterAroundStyle } from "@Style/.";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  width: 80%;
+  height: 70%;
+  margin-top: 5vh;
+  ${CenterAroundStyle}
+`;
+
+export const TeamImg = styled.img`
+  width: 35vw;
+`;
