e4309a92b018868bf9433733c050a3c628640602
Noelsky <hansorly@ajou.ac.kr>
2022-05-03 23:50:15 +0900

    Feat : About team 추가

--- client/src/Components/Molecules/About/TeamInfo/styles.tsx
+++ client/src/Components/Molecules/About/TeamInfo/styles.tsx
@@ -5,10 +5,24 @@ export const Container = styled.div`
   display: flex;
   width: 80%;
   height: 70%;
-  margin-top: 5vh;
+  margin-top: 2vh;
   ${CenterAroundStyle}
+  & .arrow {
+    &:hover {
+      cursor: pointer;
+      opacity: 0.5;
+      transition: 0.5s;
+    }
+  }
 `;
 
 export const TeamImg = styled.img`
   width: 35vw;
+`;
+
+export const CircleContainer = styled.div`
+  margin-top: 2vh;
+  width: 20%;
+  display: flex;
+  ${CenterAroundStyle}
 `;
