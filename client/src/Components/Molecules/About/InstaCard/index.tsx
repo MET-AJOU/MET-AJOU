e1a56a05a42da0801db8d97bf6d8cd0b573577a8
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-05-03 19:33:50 +0900

    Feat : About work UI 추가

---
+++ client/src/Components/Molecules/About/InstaCard/index.tsx
@@ -0,0 +1,7 @@
+import { Container } from "./styles";
+
+const InstaCard = ({ className }: { className: string }) => {
+  return <Container className={className} />;
+};
+
+export default InstaCard;
---
+++ client/src/Components/Molecules/About/InstaCard/styles.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 90%;
+  height: 95%;
+  background: #ffffff;
+  margin: auto;
+  border-radius: 15px;
+  position: relative;
+  top: 0;
+  transition: 1s;
+`;
