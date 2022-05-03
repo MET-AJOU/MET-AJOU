ed21ebe4ac35aa645912960c10d536cc1cd1d752
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-05-03 19:36:04 +0900

    Feat : About Story 추가

--- client/src/Components/Organisms/About/Story/index.tsx
+++ client/src/Components/Organisms/About/Story/index.tsx
@@ -1,10 +1,48 @@
-import { AboutStoryContainer } from "./styles";
+import { useInView } from "react-hook-inview";
+
+import { GreyText } from "@Atoms/Text";
+import AboutTitle from "@Atoms/AboutTitle";
+import useAnimation from "./useAnimation";
+
+import { AboutStoryContainer, Text, TextBoxContainer, CharacterContainer, ImageTextContainer, SmallText, CharacterImgStyle, ImgStyles } from "./styles";
 
 const AboutStory = () => {
+  const [ref, isVisible] = useInView();
+  useAnimation(isVisible);
+
   return (
-    <AboutStoryContainer>
-      <p>우리의 이야기</p>
-      <p>우리가 누구인가?</p>
+    <AboutStoryContainer className="About_Story">
+      <AboutTitle>Our Story</AboutTitle>
+      <GreyText>프로젝트를 시작하게 된 계기를 소개합니다</GreyText>
+      <Text>메타버스를 활용하여 대면수업의 여부와 상관없이,</Text>
+      <Text>아주대학교 재학생들이 지속적으로 학교생활을 영위할 수 있는 세계를 만들고자 합니다.</Text>
+      <TextBoxContainer ref={ref}>
+        <img className="scale_down" src="/asset/About/text_box_white.svg" alt="white_text_box" style={ImgStyles} />
+        <img className="scale_down" src="/asset/About/text_box_blue.svg" alt="blue_text_box" style={ImgStyles} />
+        <img className="scale_down" src="/asset/About/text_box_white.svg" alt="white_text_box" style={ImgStyles} />
+      </TextBoxContainer>
+      <ImageTextContainer>
+        <SmallText className="slide_down">
+          우리 학교에 최적화된 새로운 커뮤니티
+          <br />
+          플랫폼이 있었으면 좋겠어요!
+        </SmallText>
+        <SmallText className="slide_down">
+          온라인으로도 친구들과 뛰어 놀며
+          <br />
+          새로운 추억을 만들고 싶어요!
+        </SmallText>
+        <SmallText className="slide_down">
+          비대면 상황으로 인해 못 가본 학교를
+          <br />
+          간접적으로 체험해보고 싶어요!
+        </SmallText>
+      </ImageTextContainer>
+      <CharacterContainer>
+        <img src="/asset/About/character1.svg" alt="character1" style={CharacterImgStyle} />
+        <img src="/asset/About/character2.svg" alt="character2" style={CharacterImgStyle} />
+        <img src="/asset/About/character3.svg" alt="character3" style={CharacterImgStyle} />
+      </CharacterContainer>
     </AboutStoryContainer>
   );
 };
--- client/src/Components/Organisms/About/Story/styles.tsx
+++ client/src/Components/Organisms/About/Story/styles.tsx
@@ -1,10 +1,59 @@
 import styled from "styled-components";
 
+import { FlexColumnStyle, CenterAroundStyle } from "@Style/.";
+import { InlineStyleType } from "@Type/.";
+
 export const AboutStoryContainer = styled.div`
-  width: 100vw;
-  height: 400px;
-  display: flex;
-  flex-direction: column;
+  width: 90vw;
+  height: 70vh;
+  margin: auto;
+  margin-top: 30vh;
+  ${FlexColumnStyle}
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
 `;
+
+export const Text = styled.span`
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 40px;
+`;
+
+export const TextBoxContainer = styled.div`
+  width: 80%;
+  display: flex;
+  margin: auto;
+  ${CenterAroundStyle}
+`;
+
+export const CharacterContainer = styled.div`
+  width: 80%;
+  display: flex;
+  margin: auto;
+  margin-top: 8vh;
+  ${CenterAroundStyle}
+`;
+export const ImageTextContainer = styled.div`
+  width: 80%;
+  display: flex;
+  margin: auto;
+  margin-top: -11vh;
+  text-align: center;
+  min-height: 1vh;
+  ${CenterAroundStyle}
+`;
+export const SmallText = styled.span`
+  font-size: 0.7rem;
+  font-weight: 600;
+  line-height: 2vh;
+  transition: 1s;
+`;
+
+export const ImgStyles: InlineStyleType = {
+  width: "20%",
+  transition: "1s",
+};
+
+export const CharacterImgStyle: InlineStyleType = {
+  width: "10%",
+};
