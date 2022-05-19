7e6ded2ad93b3e819eb1c26a94bd311fe067eafd
jin-Pro <dnjun2@ajou.ac.kr>
2022-05-20 04:01:50 +0900

    fix: Character Selector S3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,13 @@
+import { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import PublicRoute from "@Route/PublicRoute";
 import MainPage from "@Pages/Main";
 import LoginPage from "@Pages/Login";
 import MapPage from "@Pages/Map";
 import AboutPage from "@Pages/About";
-import { Suspense } from "react";
 import SelectModePage from "@Pages/SelectMode";
 import DebatePage from "@Pages/Debate";
 import CheckUserRoute from "@Route/CheckUserRoute";
-// import MobileView from "@Pages/Mobile";
-// import CheckUserRoute from "@Route/CheckUserRoute";
-// import RegisterPage from "@Pages/Register";
-// import PrivacyPage from "@Pages/Privacy";
-// import NickNamePage from "@Pages/NickName";
-// import CharacterPage from "@Pages/Character";
-// import ChannelPage from "@Pages/Channel";
 
 const App = () => {
   // if (window.innerWidth < 768) return <MobileView />;
@@ -30,11 +23,6 @@ const App = () => {
         <Route path="/nickName" element={<CheckUserRoute />} />
         <Route path="/character" element={<CheckUserRoute />} />
         <Route path="/Channel" element={<CheckUserRoute />} />
-        {/* <Route path="/verify" element={<PublicRoute component={RegisterPage} />} /> */}
-        {/* <Route path="/privacy" element={<PublicRoute component={PrivacyPage} />} /> */}
-        {/* <Route path="/nickName" element={<PublicRoute component={NickNamePage} />} /> */}
-        {/* <Route path="/character" element={<PublicRoute component={CharacterPage} />} /> */}
-        {/* <Route path="/Channel" element={<PublicRoute component={ChannelPage} />} /> */}
         <Route path="/map" element={<PublicRoute component={MapPage} />} />
         <Route path="/debate" element={<PublicRoute component={DebatePage} />} />
       </Routes>
