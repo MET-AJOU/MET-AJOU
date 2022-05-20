e63b831755eb14c8b7dfa169d2004ef835f74a24
jin-Pro <dnjun2@ajou.ac.kr>
2022-05-20 19:35:52 +0900

    fix: delete 주석

--- client/src/Components/Organisms/Privacy/usePrivacyHook.tsx
+++ client/src/Components/Organisms/Privacy/usePrivacyHook.tsx
@@ -5,30 +5,16 @@ import { userDataAtom } from "@Recoils/UserData";
 import { useCallback, useRef, useState } from "react";
 import { useSetRecoilState } from "recoil";
 import { setHandleMoveNext } from "./util";
-// import { checkBoxCheck, setHandleChecked, setHandleMoveNext, handleVerifyUseable } from "./util";
 
 const usePrivacyHook = () => {
   const setUserData = useSetRecoilState(userDataAtom);
   const [next, setNext] = useState(true);
-  // const [checked, setChecked] = useState(INIT_CHECKED);
   const inputRefs = useRef<HTMLInputElement[]>([]);
   const nextPage = useMovePage(NICKNAME);
 
   const handleMoveNext = useCallback(setHandleMoveNext({ inputRefs, setNext, nextPage, setUserData }), []);
-  // const handleChecked = useCallback(setHandleChecked({ inputRefs, setChecked }), []);
 
   return { next, inputRefs, handleMoveNext };
-  // return { next, inputRefs, handleMoveNext, handleChecked };
 };
 
 export default usePrivacyHook;
-
-// const INIT_CHECKED: checkedType = {
-//   0: false,
-//   1: false,
-// };
-
-// export interface checkedType {
-//   0: boolean;
-//   1: boolean;
-// }
