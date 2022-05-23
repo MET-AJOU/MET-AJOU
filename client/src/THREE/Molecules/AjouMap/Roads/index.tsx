24056082c9bd939cd9d84d8460eb1970b1ea5230
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-05-23 16:06:57 +0900

    etc: road asset update

--- client/src/THREE/Molecules/AjouMap/Roads/index.tsx
+++ client/src/THREE/Molecules/AjouMap/Roads/index.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/no-array-index-key */
 import { RoadsAssetSize, RoadsSrc } from "@Constant/Three";
-import Ground from "@THREE/Atoms/Ground";
+import FBXs from "@THREE/Atoms/Fbxs";
 
 const Roads = () => {
   return (
     <>
       {new Array(RoadsAssetSize).fill(1).map((_, idx) => (
-        <Ground key={idx} src={`${RoadsSrc}/road_${idx + 1}.gltf`} 키="road" />
+        <FBXs key={`roads${idx}`} src={`${RoadsSrc}/road_${String(idx + 1).padStart(2, "0")}.fbx`} />
       ))}
     </>
   );
