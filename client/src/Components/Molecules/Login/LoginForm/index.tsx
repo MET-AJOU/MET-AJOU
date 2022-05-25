12e4ad38a4640b483a6394cce3d71483e9592ecd
jin-Pro <dnjun2@ajou.ac.kr>
2022-05-25 21:33:04 +0900

    enhance: KAKAO Login

--- client/src/Components/Molecules/Login/LoginForm/index.tsx
+++ client/src/Components/Molecules/Login/LoginForm/index.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable no-return-assign */
-import { GOOGLE_URL } from "@Constant/URL";
+import { GOOGLE_URL, KAKAO_URL } from "@Constant/URL";
 import LoginButton from "@Atoms/LoginRegister/Login/LoginButton";
 import { LoginFormContainer } from "./styles";
 
 const LoginForm = () => {
-  const handleLoginClick = () => (window.location.href = GOOGLE_URL);
+  const handleLoginClick = (url: string) => () => (window.location.href = url);
   return (
     <LoginFormContainer>
-      <LoginButton fn={undefined} title="KaKao로 로그인" />
-      <LoginButton fn={handleLoginClick} title="Google로 로그인" />
+      <LoginButton fn={handleLoginClick(KAKAO_URL)} title="KaKao로 로그인" />
+      <LoginButton fn={handleLoginClick(GOOGLE_URL)} title="Google로 로그인" />
     </LoginFormContainer>
   );
 };
