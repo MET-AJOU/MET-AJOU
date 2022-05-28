06f9937ee843d6f6b079cee9bce8c31838e44a87
Noelsky <hansorly@ajou.ac.kr>
2022-05-28 20:04:55 +0900

    etc: 체육관 관련 상수 선언

---
+++ client/src/Common/Constant/Three/Gym/index.ts
@@ -0,0 +1,23 @@
+export const GYM_ASSET_URL = "./models/Gym";
+
+export const GYM_ASSET_SIZE = 0.15;
+
+export const CEILING_ASSET_SIZE = 1;
+
+export const CEILING_SRC = `${GYM_ASSET_URL}/Ceiling`;
+
+export const CURTAIN_ASSET_SIZE = 1;
+
+export const CURTAIN_SRC = `${GYM_ASSET_URL}/Curtain`;
+
+export const FLOOR_ASSET_SIZE = 1;
+
+export const FLOOR_SRC = `${GYM_ASSET_URL}/Floor`;
+
+export const STAIR_ASSET_SIZE = 1;
+
+export const STAIR_SRC = `${GYM_ASSET_URL}/Stair`;
+
+export const WALL_ASSET_SIZE = 4;
+
+export const WALL_SRC = `${GYM_ASSET_URL}/Wall`;
