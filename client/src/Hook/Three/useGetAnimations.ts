9753b42446ef9a831dfed78a6f310ffee5e928be
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-06-01 00:00:34 +0900

    refactor: 애니메이션 추가 확장성 고려

--- client/src/Hook/Three/useGetAnimations.ts
+++ client/src/Hook/Three/useGetAnimations.ts
@@ -3,20 +3,13 @@ import { useFBX, useAnimations } from "@react-three/drei";
 interface animationSrcType {
   [index: string]: string;
 }
-const useGetAnimations = ({ animationSrcs, ref }: { animationSrcs: animationSrcType; ref: any }) => {
-  const walkingAnimation = useFBX(animationSrcs.walkingSrc);
-  const runningAnimation = useFBX(animationSrcs.runningSrc);
-  const dancingAnimation = useFBX(animationSrcs.dancingSrc);
-  const jumpingAnimation = useFBX(animationSrcs.jumpingSrc);
-  const { animations: walkingAnimations } = walkingAnimation;
-  const { animations: runningAnimations } = runningAnimation;
-  const { animations: dancingAnimations } = dancingAnimation;
-  const { animations: jumpingAnimations } = jumpingAnimation;
-  const { actions: walkingActions } = useAnimations(walkingAnimations, ref);
-  const { actions: runningActions } = useAnimations(runningAnimations, ref);
-  const { actions: dancingActions } = useAnimations(dancingAnimations, ref);
-  const { actions: jumpingActions } = useAnimations(jumpingAnimations, ref);
-  return { walkingActions, runningActions, dancingActions, jumpingActions };
-};
+const useGetAnimations = ({ animationSrcs, ref }: { animationSrcs: animationSrcType; ref: any }) => makeActions({ animationSrcs, ref });
+
+const makeActions = ({ animationSrcs, ref }: { animationSrcs: animationSrcType; ref: any }) =>
+  Object.entries(animationSrcs).reduce((acc: object, [key, src]) => {
+    const { animations } = useFBX(src);
+    const { actions } = useAnimations(animations, ref);
+    return { ...acc, [`${key}Actions`]: actions };
+  }, {});
 
 export default useGetAnimations;
