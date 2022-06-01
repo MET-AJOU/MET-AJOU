b5bdf720784f543b8bdd0d4bfb720d8d540e4ddb
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-06-01 21:27:52 +0900

    feat : 닉네임 변경 불가

--- client/src/THREE/Atoms/Socket/index.tsx
+++ client/src/THREE/Atoms/Socket/index.tsx
@@ -1,6 +1,7 @@
 import { routingType } from "@Route/util";
 import useInitSocket from "@Socket/Hook/useInitSocket";
 import { ChatType } from "@Type/Three";
+import React from "react";
 import { SetterOrUpdater } from "recoil";
 
 const SocketComponent = ({ setJoinedUserNumber, roomId, setOutUser, setChatInfos, userData }: { setJoinedUserNumber: React.Dispatch<React.SetStateAction<number>>; roomId: number; setOutUser: SetterOrUpdater<routingType | null>; setChatInfos: SetterOrUpdater<ChatType[]>; userData: routingType | null }) => {
@@ -8,4 +9,4 @@ const SocketComponent = ({ setJoinedUserNumber, roomId, setOutUser, setChatInfos
   return null;
 };
 
-export default SocketComponent;
+export default React.memo(SocketComponent);
