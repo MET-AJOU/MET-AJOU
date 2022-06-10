c5bc143080b0391c119c4c2172fdcee9837f04cd
jin-Pro <dnjun2@ajou.ac.kr>
2022-06-10 14:49:09 +0900

    fix: mobile character delete

--- client/src/THREE/Atoms/Socket/index.tsx
+++ client/src/THREE/Atoms/Socket/index.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { SetterOrUpdater } from "recoil";
 
 const SocketComponent = ({ setJoinedUserNumber, roomId, setOutUser, setChatInfos, userData }: { setJoinedUserNumber: React.Dispatch<React.SetStateAction<number>>; roomId: number; setOutUser: SetterOrUpdater<routingType | null>; setChatInfos: SetterOrUpdater<ChatType[]>; userData: routingType | null }) => {
+  if (window.innerWidth < 768) return null;
   useInitSocket({ setJoinedUserNumber, roomId, setOutUser, setChatInfos, userData });
   return null;
 };
