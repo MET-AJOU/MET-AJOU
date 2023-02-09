e42403df558bd9ea723bedbc94fc63b9d918e4e5
Noelsky <hansorly@ajou.ac.kr>
2023-02-09 09:28:21 +0900

    enhance: 닉네임 페이지 관련파일 제거

--- client/src/Components/Molecules/NickName/Title/index.tsx
+++
@@ -1,7 +0,0 @@
-import { NickNameTitleContainer } from "./styles";
-
-const NickNameTitle = () => {
-  return <NickNameTitleContainer>닉네임을 입력하세요</NickNameTitleContainer>;
-};
-
-export default NickNameTitle;
